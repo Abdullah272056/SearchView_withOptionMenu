71fb0a6c2a62d8ce6d62e68f2bec4fda266bbe86
Abdullah272056 <abdullah272056@gmail.com>
2020-02-23 03:20:27 +0600

    Initial commit

---
+++ app/src/main/java/com/example/searchview_withoptionmenu/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.searchview_withoptionmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[]countryName;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listViewId);
+        countryName=getResources().getStringArray(R.array.country_name);
+
+         adapter=new ArrayAdapter<String >(MainActivity.this,R.layout.sample,R.id.text_view_id,countryName);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        MenuItem menuItem=menu.findItem(R.id.searchItemId);
+        SearchView searchView= (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+
+}
